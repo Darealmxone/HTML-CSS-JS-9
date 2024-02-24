647e3bc1c9569cd5a620b43a333f248f5df73580
Darealmxone <93681347+Darealmxone@users.noreply.github.com>
2024-02-24 01:46:55 +0000

    Added typewrite effect added js functionality to modify typewrite position based on window resize and canvas element.

---
+++ type/type.js
@@ -0,0 +1,74 @@
+// Typewriter effect setup
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+// Function to center the h1 element
+function centerElementOnCanvas() {
+    var canvas = document.getElementById('myCanvas');
+    var h1 = document.querySelector('.typewrite');
+    h1.style.position = 'absolute'; // Ensure the h1 element is positioned absolutely
+    h1.style.top = (window.innerHeight / 2 - h1.offsetHeight / 2) + 'px';
+    h1.style.left = (window.innerWidth / 2 - h1.offsetWidth / 2) + 'px';
+}
+
+// Initialize typewriter effect and centering on window load
+window.onload = function() {
+    // Typewriter effect initialization
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // Center the h1 element
+    centerElementOnCanvas();
+
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+// Center the h1 element on resize
+window.addEventListener('resize', centerElementOnCanvas);
